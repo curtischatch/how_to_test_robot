4ec56ceaeb329123bf21e56eefd11588a6efacc7
curtischatch <curtis.chat.hk@gmail.com>
2018-08-24 17:42:28 -0400

    finished 1-4

--- test_robot.rb
+++ test_robot.rb
@@ -4,45 +4,61 @@ require './robot.rb'
 
 class TestRobot < MiniTest::Test
 
+  def setup
+    @robot = Robot.new
+  end
+
   def test_that_foreign_robot_needing_repairs_sent_to_station_1
-    skip
-    # arrange
 
-    # act
+    @robot.needs_repairs = true
+    @robot.foreign_model = 'honda'
 
-    # assert
+    expected_value = 1
+    actual_value = @robot.station
+
+    assert_equal(expected_value, actual_value)
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
-    # arrange
+
+    @robot.needs_repairs = true
+    @robot.vintage_model = 'kia'
 
     # act
+    expected_value = 2
+    actual_value = @robot.station
 
     # assert
+    assert_equal(expected_value, actual_value)
+
   end
 
   def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
-    # arrange
+
+    @robot.needs_repairs = true
 
     # act
+    expected_value = 3
+    actual_value = @robot.station
 
     # assert
+    assert_equal(expected_value, actual_value)
   end
 
   def test_that_robot_in_good_condition_sent_to_station_4
-    skip
-    # arrange
+
 
     # act
+    expected_value = 4
+    actual_value = @robot.station
 
     # assert
+    assert_equal(expected_value, actual_value)
   end
 
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
-    skip
-    # arrange
+
+    validates :presence, true
 
     # act
 
